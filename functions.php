8f6944b3350b30ab82ee1ce482e581bdbc612677
Andy Fragen <andy@thefragens.com>
2012-07-29 09:10:41 -0700

    cleanup

--- functions.php
+++ functions.php
@@ -1,20 +1,15 @@
 <?php
 
-//add_action('wp_footer', 'sts_address');
-function sts_address() {
-  //$content = '<span id="sts-address">455 S. Compadre Road, Palm Springs, CA 92262<br />Phone: 760-327-4919 &bull; Fax: 760-327-4429</span>';
-  //echo $content;
-}
 
 add_filter( 'bloginfo', 'my_tagline', 10, 2 );
-add_filter( 'bloginfo', 'my_name', 10, 2 );
 function my_tagline( $text, $show ) {
     if( 'description' == $show ) {
         $text = '<span id="tag2">Saturday, May 19, 2012</span> <span id="tag1"><a href="http://www.indiancanyonsgolf.com" target="_blank" title="Click for directions">Indian Canyons Golf Resort</a></span>';
     }
     return $text;
-} 
+}
 
+add_filter( 'bloginfo', 'my_name', 10, 2 );
 function my_name( $text, $show ) {
 	if( 'name' == $show ) {
 		$text = '<span id="ps">Palm Springs</span><span id="smooth">Smooth</span><span id="fest">Festival XVII</span><span id="jazz">Jazz</span>';
@@ -30,35 +25,5 @@ function jazz_contact() {
   }
   echo $content;
 }
-
-
-// function dmm_nav_menu() { 
-//   if ( function_exists( 'wp_nav_menu' ) )
-//     if ( is_home() || is_front_page() ) {
-//       wp_nav_menu( array( 
-//         'menu_class' => 'nav-menu', 
-//         'theme_location' => 'top-menu', 
-//         'fallback_cb' => ''
-//          ) );
-//     } else {
-//       wp_nav_menu( array( 
-//         'menu_class' => 'nav-menu', 
-//         'theme_location' => 'secondary-menu', 
-//         'fallback_cb' => '' 
-//         ) );
-//     }
-//   else
-//     wp_list_pages();
-// }
-// 
-// add_action( 'init', 'register_dmm_menu' );
-// function register_dmm_menu() {
-//   register_nav_menus(
-//     array(
-//       'top-menu' => __( 'Top Menu' ),
-//       'secondary-menu' => __( 'Secondary Menu' )
-//     )
-//   );
-// }
 
  ?>
