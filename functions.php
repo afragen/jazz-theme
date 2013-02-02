0b2e78a5853a126ace93a473b4be1a0fcf61aa6d
Andy Fragen <andy@thefragens.com>
2013-02-02 12:42:00 -0800

    add city, add tags to css

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 add_filter( 'bloginfo', 'location_tagline', 10, 2 );
 function location_tagline( $text, $show ) {
     if( 'description' == $show ) {
-        $text = '<span id="tag2">Saturday, May 19, 2012</span> <span id="tag1"><a href="http://www.indiancanyonsgolf.com" target="_blank" title="Click for directions">Indian Canyons Golf Resort</a></span>';
+        $text = '<span id="tag2">Saturday, May 18, 2013</span> <span id="city">Palm Springs, California</span> <span id="tag1"><a href="http://www.indiancanyonsgolf.com" target="_blank" title="Click for directions">Indian Canyons Golf Resort</a></span>';
     }
     return $text;
 }
@@ -20,7 +20,7 @@ function pssjf_name( $text, $show ) {
 add_filter( 'bloginfo', 'finc_name', 10, 2 );
 function finc_name( $text, $show ) {
 	if( 'name' == $show ) {
-		$text = '<span id="name1">Festival</span><span id="name3">in the</span><span id="name3">Canyons</span><span id="year">XVIII</span>';
+		$text = '<span id="festival">Festival</span><span id="inthe">in the</span><span id="canyons">Canyons</span><span id="year">XVIII</span>';
 	}
 	return $text;
 }
